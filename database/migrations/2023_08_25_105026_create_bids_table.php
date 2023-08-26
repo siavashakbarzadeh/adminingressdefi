6fb4194d5ea21a05f0e05f1242bf07d8242c95e9
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-08-26 17:59:50 +0200

    etgwe

--- database/migrations/2023_08_25_105026_create_bids_table.php
+++ database/migrations/2023_08_25_105026_create_bids_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('bids', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('nft_id')->constrained('nfts')->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->decimal('bid_amount', 20, 2)->nullable();
+            $table->integer('is_accepted')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('finished_date')->nullable();
             $table->timestamps();
         });
     }
